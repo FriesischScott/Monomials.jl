e0433b136806ad769511a9aa891b77cc4df618d5
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2024-08-27 15:50:45 +0200

    Update show for degrees above 10

--- src/show.jl
+++ src/show.jl
@@ -1,4 +1,4 @@
-const superscripts = ("¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹")
+const superscripts = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
 
 function Base.show(io::IO, m::Monomial)
     str = map(zip(m.x, m.α)) do (x, d)
@@ -7,7 +7,11 @@ function Base.show(io::IO, m::Monomial)
         elseif d == 1
             return x
         else
-            return join([x, superscripts[d]])
+            if d < 10
+                return join([x, superscripts[d+1]])
+            else
+                return join([x, superscripts[parse.(Int, split(string(d), "")).+1]...])
+            end
         end
     end
 
